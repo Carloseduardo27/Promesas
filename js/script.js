3156ed8afc171d1abcf24af061be6d1efa27d0be
carlos perez <carlosperez27041990@gmail.com>
2025-06-02 08:45:18 -0400

    error en el boton

--- js/script.js
+++ js/script.js
@@ -3,17 +3,14 @@ const botonIniciarPedido = document.getElementById('iniciarPedido');
 const mensajes = document.getElementById('mensajes');
 
 //Simula la preparación de un platillo con un retardo.
-
 const simularPreparacion = (platillo) => {
     return new Promise((resolve) => {
-        // Añadir mensaje de inicio de preparación
         mensajes.innerHTML += `<p>Preparando ${platillo}...</p>`;
         // Simular un tiempo de preparación de 2 segundos
         setTimeout(() => {
-            // Añadir mensaje de platillo listo
             mensajes.innerHTML += `<p>${platillo} listo.</p>`;
-            resolve(); // Resolver la promesa
-        }, 2000); // 2 segundos de simulación
+            resolve(); 
+        }, 2000);
     });
 };
 
@@ -21,9 +18,9 @@ const simularPreparacion = (platillo) => {
 botonIniciarPedido.addEventListener('click', () => {
     // Limpiar mensajes anteriores al iniciar un nuevo pedido
     mensajes.innerHTML = '';
-    // Deshabilitar el botón mientras se procesa el pedido
     botonIniciarPedido.disabled = true;
     botonIniciarPedido.textContent = 'Procesando pedido...';
+
 
     // Encadenar las promesas para simular la secuencia de preparación
     simularPreparacion('bebida')
@@ -35,11 +32,9 @@ botonIniciarPedido.addEventListener('click', () => {
             console.log('¡Orden completa entregada!');
         })
         .catch((error) => {
-            // Manejo de errores si alguna promesa falla
             mensajes.innerHTML += `<p>Ocurrió un error: ${error.message}</p>`;
         })
         .finally(() => {
-            // Habilitar el botón de nuevo al finalizar el proceso
             botonIniciarPedido.disabled = false;
             botonIniciarPedido.textContent = 'Iniciar Pedido';
         });
